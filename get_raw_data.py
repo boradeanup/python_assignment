26ccd82150d90cc7167dbc8c8faf761f632a500c
boradeanup <boradeanup@gmail.com>
2023-04-03 08:58:53 +0900

    fetch data within 2 weeks

--- get_raw_data.py
+++ get_raw_data.py
@@ -2,6 +2,8 @@ import requests
 import os
 from dotenv import load_dotenv
 import json
+from datetime import datetime, timedelta
+
 
 
 load_dotenv('./financial/.env')
@@ -10,6 +12,17 @@ API_KEY = os.getenv('API_KEY')
 
 company_list = ['IBM' , 'AAPL']
 # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+
+
+def is_within_two_weeks(date_str):
+    try:
+        date = datetime.strptime(date_str, "%Y-%m-%d")
+        today = datetime.today()
+        two_weeks_ago = today - timedelta(days=14)
+        return two_weeks_ago <= date and date <= today
+    except:
+        print('Invalid date format')
+        return False
 
 for company in company_list:
     url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&apikey={}&symbol={}'.format(API_KEY, company)
@@ -27,13 +40,14 @@ for company in company_list:
         close_price = values["4. close"]
         volume = values["6. volume"]
         
-        # Append the processed data to the list
-        processed_data.append({
-            "symbol": company,
-            "date": date,
-            "open_price": open_price,
-            "close_price": close_price,
-            "volume": volume
-        })
+        if(is_within_two_weeks(date)):
+            # Append the processed data to the list
+            processed_data.append({
+                "symbol": company,
+                "date": date,
+                "open_price": open_price,
+                "close_price": close_price,
+                "volume": volume
+            })
 
         print(processed_data)
