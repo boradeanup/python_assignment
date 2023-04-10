ceecd407c04b3d1c678287f93dc8932c8f786aa1
boradeanup <boradeanup@gmail.com>
2023-04-10 14:41:35 +0900

    add blueprint, comments

--- financial/app.py
+++ financial/app.py
@@ -3,27 +3,26 @@ from . import app
 from .services import (validate_query_params, get_financial_data, InvalidQueryParameter,
                        is_company_symbol, calculate_statistics, get_stats_financial_data,
                        validate_stats_input)
-from flask import jsonify, request
+from flask import jsonify, request, Blueprint
 import datetime
 
+api_bp = Blueprint('api', __name__, url_prefix='/api')
 
 
-
-@app.route('/')
+@api_bp.route('/')
 def hello():
     return {"hello": "world"}
 
 
 # Custom error handler for invalid query parameters
-@app.errorhandler(InvalidQueryParameter)
+@api_bp.errorhandler(InvalidQueryParameter)
 def handle_invalid_query_parameter(error):
     response = jsonify({"data": [], "pagination": {}, 'info': {'error':str(error)}})
     response.status_code = 400
     return response
 
 
-
-@app.route('/financial_data')
+@api_bp.route('/financial_data')
 def get():
     # Get the query parameters from the request with default values
     symbol = request.args.get('symbol', '')
@@ -42,32 +41,45 @@ def get():
     return jsonify(response_data)
 
 
-@app.route('/statistics')
+@api_bp.route('/statistics')
 def get_statistics():
+    # Retrieve query parameters from request
     start_date_str = request.args.get('start_date')
     end_date_str = request.args.get('end_date')
     symbol = request.args.get('symbol')
 
+    # Validate query parameters
     error, status_code = validate_stats_input(start_date_str, end_date_str, symbol)
     if error:
+        # Return error response if validation fails
         return jsonify({'data': {}, 'info': {'error': error}}), status_code
 
+    # Convert date strings to datetime objects
     start_date = datetime.datetime.strptime(start_date_str, '%Y-%m-%d')
     end_date = datetime.datetime.strptime(end_date_str, '%Y-%m-%d')
+    
+    # Retrieve financial data based on query parameters
     financial_data = get_stats_financial_data(symbol, start_date, end_date)
 
+    # Return error response if no financial data is found for the given symbol and period
     if not financial_data:
         return jsonify({'data': {}, 'info': {'error': 'No financial data found for the given symbol and period.'}})
 
+    # Calculate statistics based on financial data
     response_data = calculate_statistics(financial_data)
+    
+    # Add additional fields to response data
     response_data.update({
         'start_date': start_date.strftime('%Y-%m-%d'),
         'end_date': end_date.strftime('%Y-%m-%d'),
         'symbol': symbol
     })
 
+    # Return response as JSON
     return jsonify({'data': response_data, 'info': {'error': ''}})
 
+# Register the blueprint
+app.register_blueprint(api_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
