4ed3076071119230f68f195688ffc45f45ede0dd
boradeanup <boradeanup@gmail.com>
2023-04-10 14:05:06 +0900

    implement statistics API

--- financial/app.py
+++ financial/app.py
@@ -46,7 +46,7 @@ def get():
 
     # Check data types of query parameters if they are not None
     if symbol and len(symbol)>0 and not is_company_symbol(symbol):
-        raise InvalidQueryParameter('symbol should be one of AAPL, IBM')
+        raise InvalidQueryParameter('symbol should be one of AAPL(Apple Inc.), IBM')
     if start_date and is_date_error(start_date):
         raise InvalidQueryParameter('date must be a of format YY-mm-dd')
     if end_date and is_date_error(end_date):
@@ -109,6 +109,64 @@ def get():
 
     return jsonify(response)
 
+# error hanlder class somewhere
+# helper methods somewhere
+# function 1: create json response
+# function 2: input query param check
+# function 3: get data for this. . . 
+# functino 4: generate paginatio data.
+
+
+@app.route('/statistics')
+def get_statistics():
+    # Get input parameters
+    start_date_str = request.args.get('start_date')
+    end_date_str = request.args.get('end_date')
+    symbol = request.args.get('symbol')
+
+    # Validate input parameters
+    if not start_date_str or not end_date_str or not symbol:
+        return jsonify({'data': {},'info': {'error': 'Missing required parameters'}}), 400
+    
+    try:
+        start_date = datetime.datetime.strptime(start_date_str, '%Y-%m-%d')
+        end_date = datetime.datetime.strptime(end_date_str, '%Y-%m-%d')
+    except ValueError:
+        return jsonify({'data': {},'info': {'error': 'Invalid date format'}}), 400
+
+    if start_date > end_date:
+        return jsonify({'data': {},'info': {'error': 'Invalid date range'}}), 400
+
+    if not is_company_symbol(symbol):
+        return jsonify({'data': {},'info': {'error': 'symbol should be one of AAPL(Apple Inc.), IBM'}}), 400
+
+    financial_data = FinancialData.query.filter_by(symbol=symbol).\
+                    filter(FinancialData.date >= start_date.date()).\
+                    filter(FinancialData.date <= end_date.date()).all()
+
+    if not financial_data:
+        return jsonify({'data': {}, 'info': {'error': 'No financial data found for the given symbol and period.'}})
+
+    # Calculate statistics
+    total_open_price = sum([float(fd.open_price) for fd in financial_data])
+    total_close_price = sum([float(fd.close_price) for fd in financial_data])
+    total_volume = sum([int(fd.volume) for fd in financial_data])
+    num_days = len(financial_data)
+    average_daily_open_price = total_open_price / num_days
+    average_daily_close_price = total_close_price / num_days
+    average_daily_volume = total_volume / num_days
+
+    # Create response
+    response_data = {
+        'start_date': start_date.strftime('%Y-%m-%d'),
+        'end_date': end_date.strftime('%Y-%m-%d'),
+        'symbol': symbol,
+        'average_daily_open_price': average_daily_open_price,
+        'average_daily_close_price': average_daily_close_price,
+        'average_daily_volume': average_daily_volume
+    }
+
+    return jsonify({'data': response_data, 'info': {'error': ''}})
 
 
 
